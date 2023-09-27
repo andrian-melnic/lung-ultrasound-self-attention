3a8aa67bd69397e262e61a0d9948f5949845eef0
Andrian Melnic <andrianmelnic29@gmail.com>
2023-09-27 11:15:14 +0200

    Update ResNet18LightningModule.py
    Resnet18 with pre trained weights

--- ResNet18LightningModule.py
+++ ResNet18LightningModule.py
@@ -1,12 +1,14 @@
-# Model class ------------------------------------------------------------
-from torchvision import models
+from torchvision.models import resnet18, ResNet18_Weights
 import torch.nn as nn
 from torch.utils.data import DataLoader
 import pytorch_lightning as pl
 import torch
 from kornia import tensor_to_image
 import torchvision
-import matplotlib as plt
+import matplotlib.pyplot as plt
+from torchmetrics.classification import MulticlassF1Score
+import torchmetrics.functional as metrics
+
 
 from data_setup import DataAugmentation
 
@@ -28,7 +30,7 @@ class ResNet18LightningModule(pl.LightningModule):
         self.batch_size = batch_size
         self.num_workers = num_workers
 
-        self.resnet_model = models.resnet18(weights=None)
+        self.resnet_model = resnet18(weights=ResNet18_Weights.DEFAULT)
         num_features = self.resnet_model.fc.in_features
         self.resnet_model.fc = nn.Linear(num_features, num_classes)
 
@@ -36,32 +38,49 @@ class ResNet18LightningModule(pl.LightningModule):
         self.optimizer = None
 
         self.transform = DataAugmentation()
+        
+        self.f1_score_metric = MulticlassF1Score(num_classes=num_classes)
     def forward(self, x):
         return self.resnet_model(x)
 
 
     def show_batch(self, win_size=(10, 10)):
       def _to_vis(data):
+          # Ensure that pixel values are in the valid range [0, 1]
+          data = torch.clamp(data, 0, 1)
           return tensor_to_image(torchvision.utils.make_grid(data, nrow=8))
 
-      # get a batch from the training set: try with `val_datlaoader` :)
+      # Get a batch from the training set
       imgs, labels = next(iter(self.train_dataloader()))
-      imgs_aug = self.transform(imgs)  # apply transforms
-      # use matplotlib to visualize
+
+      # Apply data augmentation to the batch
+      imgs_aug = self.transform(imgs)
+
+      # Use matplotlib to visualize the original and augmented images
       plt.figure(figsize=win_size)
       plt.imshow(_to_vis(imgs))
+      plt.title("Original Images")
+
       plt.figure(figsize=win_size)
       plt.imshow(_to_vis(imgs_aug))
+      plt.title("Augmented Images")
 
-    def on_before_batch_transfer(self, batch, dataloader_idx):
+
+    def on_after_batch_transfer(self, batch, dataloader_idx):
         pixel_values, labels = batch
-        if self.trainer.training:
-            x = self.transform(pixel_values)  # => we perform GPU/Batched data augmentation
+        x = self.transform(pixel_values)  # => we perform GPU/Batched data augmentation
         return x, labels
       
     def common_step(self, batch, batch_idx):
         
+        # Initialize the Weight Transforms
+        weights = ResNet18_Weights.DEFAULT
+        preprocess = weights.transforms()
+
         pixel_values, labels = batch
+        # Apply it to the input image
+        pixel_values = preprocess(pixel_values)
+        
         logits = self(pixel_values)
 
         criterion = nn.CrossEntropyLoss()
@@ -69,23 +88,25 @@ class ResNet18LightningModule(pl.LightningModule):
         predictions = logits.argmax(dim=1)
         correct = (predictions == labels).sum().item()
         accuracy = correct / labels.size(0)
+        f1 = self.f1_score_metric(logits, labels)
 
-        return loss, accuracy
+        return loss, accuracy, f1
 
     def training_step(self, batch, batch_idx):
-        loss, accuracy = self.common_step(batch, batch_idx)
-        self.log("training_loss", loss)
-        self.log("training_accuracy", accuracy)
+        loss, accuracy, f1 = self.common_step(batch, batch_idx)
+        self.log("training_loss", loss, on_epoch=True, prog_bar=True, logger=True)
+        self.log("training_accuracy", accuracy, on_epoch=True, prog_bar=True, logger=True)
+        self.log("training_f1", f1, on_epoch=True, prog_bar=True, logger=True)
 
         return loss
 
     def test_step(self, batch, batch_idx):
-        loss, accuracy = self.common_step(batch, batch_idx)
-        self.log("test_loss", loss)
-        self.log("test_accuracy", accuracy)
+        loss, accuracy, f1 = self.common_step(batch, batch_idx)
+        self.log("test_loss", loss, on_epoch=True, prog_bar=True, logger=True)
+        self.log("test_accuracy", accuracy, on_epoch=True, prog_bar=True, logger=True)
+        self.log("test_f1", f1, on_epoch=True, prog_bar=True, logger=True)
 
         return loss
-
     def configure_optimizers(self):
         if self.optimizer_name == "adam":
             self.optimizer = torch.optim.Adam(self.parameters(), lr=self.lr)
