82bb5abcf98f637b2ee8f137e26d48e4ba4fa71c
Andrian Melnic <andrianmelnic29@gmail.com>
2023-10-23 03:10:43 +0200

    Create LUSDataModule.py

---
+++ lightning_modules/LUSDataModule.py
@@ -0,0 +1,41 @@
+import lightning as pl
+import torch
+from torch.utils.data import DataLoader
+
+
+def collate_fn(examples):
+    frames = torch.stack([example[0] for example in examples])  # Extract the preprocessed frames
+    scores = torch.tensor([example[1] for example in examples])  # Extract the scores
+    return (frames, scores)
+
+class LUSDataModule(pl.LightningDataModule):
+    def __init__(self, train_dataset, test_dataset, num_workers, batch_size):
+        super().__init__()
+        self.train_dataset = train_dataset
+        self.test_dataset = test_dataset
+        self.num_workers = num_workers
+        self.batch_size = batch_size
+        
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset,
+                          batch_size=self.batch_size,
+                          num_workers=self.num_workers,
+                          pin_memory=True,
+                          collate_fn=collate_fn, shuffle=True)
+
+    def val_dataloader(self):
+        return DataLoader(self.test_dataset,
+                          batch_size=self.batch_size,
+                          pin_memory=True,
+                          collate_fn=collate_fn)
+    # def test_dataloader(self):
+    #     return DataLoader(self.test_dataset,
+    #                       batch_size=self.batch_size,
+    #                       pin_memory=True,
+    #                       collate_fn=collate_fn)
+
+    # def test_dataloader(self):
+    #     return DataLoader(self.mnist_test, batch_size=32)
+
+    # def predict_dataloader(self):
+    #     return DataLoader(self.mnist_predict, batch_size=32)
