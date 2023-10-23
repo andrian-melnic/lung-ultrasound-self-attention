bc8775c56945055d4437838a92bf98f2a24a95a7
Andrian Melnic <andrianmelnic29@gmail.com>
2023-10-23 03:10:58 +0200

    Update LUSModelLightningModule.py

--- lightning_modules/LUSModelLightningModule.py
+++ lightning_modules/LUSModelLightningModule.py
@@ -3,7 +3,6 @@ import torchvision
 import torch.nn as nn
 import timm
 import lightning.pytorch as pl
-from torch.utils.data import DataLoader
 from kornia import tensor_to_image
 import matplotlib.pyplot as plt
 from torchmetrics.classification import MulticlassF1Score, Accuracy
@@ -12,10 +11,6 @@ from transformers import ViTForImageClassification
 
 from data_setup import DataAugmentation
 
-def collate_fn(examples):
-    frames = torch.stack([example[0] for example in examples])  # Extract the preprocessed frames
-    scores = torch.tensor([example[1] for example in examples])  # Extract the scores
-    return (frames, scores)
   
 id2label = {0: 'no', 1: 'yellow', 2: 'orange', 3: 'red'}
 label2id = {"no": 0, "yellow": 1, "orange": 2, "red": 3}
@@ -24,20 +19,14 @@ class LUSModelLightningModule(pl.LightningModule):
     def __init__(self, 
                  model_name, 
                  hparams,
-                 train_dataset,
-                 test_dataset,
-                 num_workers,
                  pretrained=True):
         
         super(LUSModelLightningModule, self).__init__()
         
 # -------------------------------- Params init ------------------------------- #
 
-        self.train_dataset = train_dataset
-        self.test_dataset = test_dataset
         
         self.num_classes = hparams['num_classes']
-        self.batch_size = hparams['batch_size']
         
         self.optim = {
             "lr": hparams['lr'],
@@ -45,7 +34,6 @@ class LUSModelLightningModule(pl.LightningModule):
             "momentum": hparams['momentum']
         }
         
-        self.num_workers = num_workers
         self.pretrained = pretrained
         
 # ----------------------------------- Model ---------------------------------- #
@@ -69,7 +57,6 @@ class LUSModelLightningModule(pl.LightningModule):
 
 
         self.optimizer_name = str(hparams['optimizer']).lower()
-        self.optimizer = None
         self.criterion = nn.CrossEntropyLoss()
         self.save_hyperparameters()
         
@@ -79,11 +66,35 @@ class LUSModelLightningModule(pl.LightningModule):
         
 # ---------------------------------- Metrics --------------------------------- #
 
-        self.f1_score_metric = MulticlassF1Score(num_classes=self.num_classes)
+        self.train_f1 = MulticlassF1Score(num_classes=self.num_classes, average="weighted")
+        self.val_f1 = MulticlassF1Score(num_classes=self.num_classes, average="weighted")
         self.train_acc = Accuracy(task='multiclass', num_classes=self.num_classes)
         self.val_acc = Accuracy(task='multiclass', num_classes=self.num_classes)
 
 # ------------------------------ Methods & Hooks ----------------------------- #
+    def configure_optimizers(self):
+        """
+        Configure and return the optimizer based on the selected optimizer name.
+
+        Returns:
+            optimizer (torch.optim.Optimizer): The configured optimizer.
+
+        Raises:
+            ValueError: If the optimizer name is invalid.
+        """
+        if self.optimizer_name == "adam":
+            optimizer = torch.optim.Adam(self.parameters(),
+                                              lr=self.optim["lr"],
+                                              weight_decay=self.optim["weight_decay"])
+        elif self.optimizer_name == "sgd":
+            optimizer = torch.optim.SGD(self.parameters(),
+                                             lr=self.optim["lr"],
+                                             momentum=self.optim["momentum"],
+                                             weight_decay=self.optim["weight_decay"])
+        else:
+            raise ValueError("Invalid optimizer name. Please choose either 'adam' or 'sgd'.")
+        
+        return optimizer
 
     def forward(self, x):
         """
@@ -94,38 +105,6 @@ class LUSModelLightningModule(pl.LightningModule):
             Tensor: The output tensor.
         """
         return self.model(x)
-
-
-    def show_batch(self, win_size=(10, 10)):
-      """
-      Displays a batch of images along with their augmented versions.
-
-      Parameters:
-          win_size (tuple): The size of the window to display the images. Defaults to (10, 10).
-
-      Returns:
-          None
-      """
-      def _to_vis(data):
-          # Ensure that pixel values are in the valid range [0, 1]
-          data = torch.clamp(data, 0, 1)
-          return tensor_to_image(torchvision.utils.make_grid(data, nrow=8))
-
-      # Get a batch from the training set
-      imgs, labels = next(iter(self.train_dataloader()))
-
-      # Apply data augmentation to the batch
-      imgs_aug = self.transform(imgs)
-
-      # Use matplotlib to visualize the original and augmented images
-      plt.figure(figsize=win_size)
-      plt.imshow(_to_vis(imgs))
-      plt.title("Original Images")
-
-      plt.figure(figsize=win_size)
-      plt.imshow(_to_vis(imgs_aug))
-      plt.title("Augmented Images")
-
 
     def on_after_batch_transfer(self, batch, dataloader_idx):
         """
@@ -158,16 +137,51 @@ class LUSModelLightningModule(pl.LightningModule):
         logits = self(x)
         loss = self.criterion(logits, y)
         self.train_acc(logits, y)
-        self.log('training_loss', loss, prog_bar=True)
-        self.log('training_accuracy', self.train_acc(logits, y), prog_bar=True)
+        self.train_f1(logits, y)
+        self.log('training_loss', loss, 
+                 prog_bar=True,
+                 on_epoch=True,
+                 logger=True)
+        self.log('training_accuracy', self.train_acc(logits, y), 
+                 prog_bar=True,
+                 on_epoch=True,
+                 logger=True)
+        self.log('training_f1', self.train_f1(logits, y), 
+                 prog_bar=True,
+                 on_epoch=True,
+                 logger=True)
         return loss
 
+    # def test_step(self, batch, batch_idx):
+    #     """
+    #     Performs a validation step on a batch of data.
+
+    #     Args:
+    #         batch (tuple): A tuple containing the input data and the correspondPing labels.
+    #         batch_idx (int): The index of the current batch.
+
+    #     Returns:
+    #         float: The loss calculated during the validation step.
+    #     """
+    #     x, y = batch
+    #     logits = self(x)
+    #     loss = self.criterion(logits, y)
+    #     self.val_acc(logits, y)
+    #     self.log('test_loss', loss, 
+    #              prog_bar=True,
+    #              on_epoch=True,
+    #              logger=True)
+    #     self.log('test_acc', self.val_acc(logits, y),
+    #              prog_bar=True,
+    #              on_epoch=True,
+    #              logger=True)
+    #     return loss
     def validation_step(self, batch, batch_idx):
         """
         Performs a validation step on a batch of data.
 
         Args:
-            batch (tuple): A tuple containing the input data and the corresponding labels.
+            batch (tuple): A tuple containing the input data and the correspondPing labels.
             batch_idx (int): The index of the current batch.
 
         Returns:
@@ -177,60 +191,38 @@ class LUSModelLightningModule(pl.LightningModule):
         logits = self(x)
         loss = self.criterion(logits, y)
         self.val_acc(logits, y)
+        self.val_f1(logits, y)
         self.log('validation_loss', loss, prog_bar=True)
         self.log('validation_acc', self.val_acc(logits, y), prog_bar=True)
+        self.log('validation_f1', self.val_f1(logits, y), prog_bar=True)
         return loss
 
-    def configure_optimizers(self):
-        """
-        Configure and return the optimizer based on the selected optimizer name.
+    def show_batch(self, win_size=(10, 10)):
+      """
+      Displays a batch of images along with their augmented versions.
 
-        Returns:
-            optimizer (torch.optim.Optimizer): The configured optimizer.
+      Parameters:
+          win_size (tuple): The size of the window to display the images. Defaults to (10, 10).
 
-        Raises:
-            ValueError: If the optimizer name is invalid.
-        """
-        if self.optimizer_name == "adam":
-            self.optimizer = torch.optim.Adam(self.parameters(),
-                                              lr=self.optim["lr"],
-                                              weight_decay=self.optim["weight_decay"])
-        elif self.optimizer_name == "sgd":
-            self.optimizer = torch.optim.SGD(self.parameters(),
-                                             lr=self.optim["lr"],
-                                             momentum=self.optim["momentum"],
-                                             weight_decay=self.optim["weight_decay"])
-        else:
-            raise ValueError("Invalid optimizer name. Please choose either 'adam' or 'sgd'.")
-        print(self.optimizer)
-        return self.optimizer
+      Returns:
+          None
+      """
+      def _to_vis(data):
+          # Ensure that pixel values are in the valid range [0, 1]
+          data = torch.clamp(data, 0, 1)
+          return tensor_to_image(torchvision.utils.make_grid(data, nrow=8))
 
-    def train_dataloader(self):
-        """
-        Returns a `DataLoader` object for training the model.
+      # Get a batch from the training set
+      imgs, labels = next(iter(self.train_dataloader()))
 
-        This function creates a `DataLoader` object using the `train_dataset` as the dataset to be loaded. The `batch_size` parameter specifies the number of samples per batch. The `collate_fn` parameter is used to collate the samples in each batch. The `shuffle` parameter indicates whether to shuffle the samples before each epoch.
+      # Apply data augmentation to the batch
+      imgs_aug = self.transform(imgs)
 
-        Returns:
-            A `DataLoader` object for training the model.
-        """
-        return DataLoader(self.train_dataset,
-                          batch_size=self.batch_size,
-                          num_workers=self.num_workers,
-                          pin_memory=True,
-                          collate_fn=collate_fn, shuffle=True)
+      # Use matplotlib to visualize the original and augmented images
+      plt.figure(figsize=win_size)
+      plt.imshow(_to_vis(imgs))
+      plt.title("Original Images")
 
-    def val_dataloader(self):
-        """
-        Returns a DataLoader object for validation data.
-        
-        Parameters:
-            self (object): The current instance of the class.
-        
-        Returns:
-            DataLoader: A DataLoader object that loads the validation dataset in batches.
-        """
-        return DataLoader(self.test_dataset,
-                          batch_size=self.batch_size,
-                          pin_memory=True,
-                          collate_fn=collate_fn)
+      plt.figure(figsize=win_size)
+      plt.imshow(_to_vis(imgs_aug))
+      plt.title("Augmented Images")
