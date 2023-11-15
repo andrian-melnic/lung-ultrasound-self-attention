34f4c5a84b12c522603814ca2fd2dfb5af9a9ee6
Andrian <andrianmelnic29@gmail.com>
2023-11-15 11:06:11 +0000

    scaling

--- data_setup.py
+++ data_setup.py
@@ -162,7 +162,7 @@ class FrameTargetDataset(Dataset):
         #     image_mean = [0.485, 0.456, 0.406]
         #     image_std = [0.229, 0.224, 0.225]
         #     frame_tensor = transforms.Normalize(mean=image_mean, std=image_std)(frame_tensor)
-        # frame_tensor = frame_tensor.float() / 255.0
+        frame_tensor = frame_tensor.float() / 255.0
         frame_tensor = frame_tensor.permute(0, 1, 2)
             
         # Target data to integer scores
