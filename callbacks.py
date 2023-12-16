4ebfbd0889bef2edf6118264737f4d350bf6fa13
Andrian <andrianmelnic29@gmail.com>
2023-12-16 23:03:56 +0000

    checkpoint and early stop on validation f1

--- callbacks.py
+++ callbacks.py
@@ -5,21 +5,21 @@ from lightning.pytorch.callbacks.progress.rich_progress import RichProgressBarTh
 
 def early_stopper():
     early_stop_callback = EarlyStopping(
-        monitor='val_loss',
+        monitor='val_f1',
         patience=15,
         strict=False,
         verbose=False,
-        mode='min'
+        mode='max'
     )
     return early_stop_callback
 
 def checkpoint_saver(checkpoint_dir):
     checkpoint_callback = ModelCheckpoint(dirpath=checkpoint_dir, 
                                           save_top_k=2,
-                                          mode="min",
-                                          monitor="val_loss",
+                                          mode="max",
+                                          monitor="val_f1",
                                           save_last=True,
                                           save_on_train_epoch_end=False,
                                           verbose=True,
-                                          filename="{epoch}-{val_acc:.4f}-{val_loss:.4f}")
+                                          filename="{epoch}-{val_acc:.4f}-{val_f1:.4f}-{val_loss:.4f}")
     return checkpoint_callback
